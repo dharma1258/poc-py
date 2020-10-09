7ce97f77c5ce0e3070e8fb2280320aaf9ef195e4
dharma <dharmachallakolchu@bitbucket.org>
2020-10-09 13:12:53 +0530

    reset broker url

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -1,4 +1,4 @@
 import os
-broker_url='amqp://test:test@172.18.0.1:5672//'
+broker_url='amqp://test:test@localhost:5672//'
 result_backend = 'rpc://'
 enable_utc = True
--- load-script.sh
+++ load-script.sh
@@ -4,7 +4,7 @@ apt-get -qq -y install python3 git
 curl -fsSL https://get.docker.com | bash -
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker run -d --hostname my-rabbit --name rabbittestuser -e RABBITMQ_DEFAULT_USER=test -e RABBITMQ_DEFAULT_PASS=test rabbitmq:3
+docker run -d --hostname my-rabbit --name rabbittestuser --network host -e RABBITMQ_DEFAULT_USER=test -e RABBITMQ_DEFAULT_PASS=test rabbitmq:3
 cd /opt
 git clone https://github.com/dharma1258/poc-py.git celery
 cd celery
