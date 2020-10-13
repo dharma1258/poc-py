14e29a914f9f4a2af1f7205c75fc2bfd75081226
dharma <dharmachallakolchu@bitbucket.org>
2020-10-13 11:32:28 +0530

    sample instance celery and flower config

--- app/celeryconfig1.py
+++ app/celeryconfig1.py
@@ -1,7 +1,5 @@
 import os
+#broker_url='amqp://test:test@localhost:5672//'
 broker_url='amqp://safecv:safecv@10.200.1.152:5672//'
 result_backend = 'rpc://'
 enable_utc = True
-task_acks_late=True
-task_reject_on_worker_lost=True
-worker_prefetch_multiplier=1
---
+++ app/flowerconfig.py
@@ -0,0 +1,7 @@
+#RabbitMQ
+
+broker_api='amqp://safecv:safecv@10.200.1.152:5672//'
+
+# Enable debug logging
+
+logging = 'DEBUG'
